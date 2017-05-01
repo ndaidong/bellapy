ec4717bb58a5287725ea94072ba4c5535b43a613
Dong Nguyen <ndaidong@gmail.com>
2017-05-01 23:28:26 +0700

    Add main

---
+++ bella/__main__.py
@@ -0,0 +1,7 @@
+import sys
+
+def main(args=None):
+  print('bella')
+
+if __name__ == '__main__':
+  main()
--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
 
   entry_points = {
     'console_scripts': [
-      'bella=bella',
+      'bella=bella.__main__:main',
     ],
   },
 )
