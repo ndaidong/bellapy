e22488841876d0fb4bb87a3ddeea03ba3bad25d7
Dong Nguyen <ndaidong@gmail.com>
2022-07-29 14:44:24 +0700

    Fix ci failed

--- run_test.sh
+++ run_test.sh
@@ -4,5 +4,5 @@ mkdir ./ptester
 poetry run flake8 ./ --exclude venv,build,dist --statistics
 poetry run safety check --bare
 # ENV=test poetry run pytest --cov-report html --cov=bella tests/*.py
-ENV=test poetry run pytest --cov-report= --cov=bella tests/*.py
+ENV=test poetry run pytest --cov=bella tests/*.py
 rm -rf ./ptester
